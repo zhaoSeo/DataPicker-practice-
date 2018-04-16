39ee53aec242c5a66e2060f724de424fa94c30f2
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-04-16 16:23:51 +0900

    1

--- DataPicker/ViewController.swift
+++ DataPicker/ViewController.swift
@@ -9,54 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let timeSelector: Selector = #selector(ViewController.updateTime)
-    let interval = 1.0
-    var count = 0
-    var alarmTime: String?
-    
-    @IBOutlet weak var lblCurrentTime: UILabel!
-    @IBOutlet weak var lblPickerTime: UILabel!
+    @IBOutlet weak var datetext: UILabel!
+    @IBOutlet weak var datepicker: UIDatePicker!
+    @IBOutlet weak var time: UILabel!
+    var timer = Timer();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
-        let datePickerView = sender
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updatetime()})
         
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
-        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
-        formatter.dateFormat = "hh:mm aaa"
-        alarmTime = formatter.string(from:datePickerView.date)
-    }
-    @objc func updateTime() {
-//        lblCurrentTime.text = String(count)
-//        count = count + 1
-     
-        let date = NSDate()
-        
-        let formatter = DateFormatter()
-        
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
-        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
-        formatter.dateFormat = "hh:mm aaa"
-        let currentTime = formatter.string(from: date as Date)
-        
-        if(alarmTime == currentTime) {
-            view.backgroundColor = UIColor.red
-        } else {
-            view.backgroundColor = UIColor.white
-        }
     }
     
-
+    @IBAction func btu(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+        
+    }
+    
+    
+    @IBAction func datepicker(_ sender: Any) {
+        let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "ko_KR")
+        dateformatter.dateFormat="yyyy-MM-dd a HH:mm:ss EEE"
+        let date = dateformatter.string(from: datepicker.date)
+        datetext.text = date
+        
+        
+        
+    }
+    func updatetime(){
+        let date1 = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy-MM-dd a HH:mm:ss EEE"
+        let time1 = formatter.string(from: date1)
+        time.text = time1
+        if datetext.text == time.text{
+            view.backgroundColor = UIColor.red
+            alert()
+        }
+        
+    }
+    func alert(){
+        let alert = UIAlertController(title: "알람", message: "취소하시겠습니까?", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: {(action: UIAlertAction!) in self.view.backgroundColor = UIColor.white})
+        let no = UIAlertAction(title: "취소", style: .default, handler:nil)
+        alert.addAction(ok)
+        alert.addAction(no)
+        
+        self.present(alert,animated: true,completion: nil)
+    }
 }
 
